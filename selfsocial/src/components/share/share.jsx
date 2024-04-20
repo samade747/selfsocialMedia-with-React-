c519eb6d139fb55d937d5214aee16903fb197893
Abdul Samad <132301326+samade747@users.noreply.github.com>
2024-04-20 20:40:52 +0500

    840pm 20 4 2024

---
+++ selfsocial/src/components/share/share.jsx
@@ -0,0 +1,66 @@
+// Importing necessary dependencies and components
+import "./share.css"; // Importing styles from share.css file
+import PermMediaIcon from '@mui/icons-material/PermMedia'; // Importing icon component from Material-UI
+import LabelIcon from '@mui/icons-material/Label'; // Importing icon component from Material-UI
+import RoomIcon from '@mui/icons-material/Room'; // Importing icon component from Material-UI
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions'; // Importing icon component from Material-UI
+import { Link } from "react-router-dom"; // Importing Link component from react-router-dom for navigation
+import { useContext, useRef, useState } from "react"; // Importing hooks from React for state management
+import { AuthContext } from "../../context/authcontext"; // Importing AuthContext for authentication
+import axios from "axios"; // Importing axios for making HTTP requests
+
+// Defining Share component
+export default function Share() {
+    
+    // using useContex hook to access the AuthContext
+    const { user } = useContext(AuthContext); // accessing user from AuthContext
+
+    // using useRef hook to create a reference for input element 
+    const desc = useRef(); // creating a reference for input element
+
+    // using useState hook to manage file state
+    const [file, setfile] = useState(); // Initializing file state variable
+
+    // Handling from submission 
+    const handleSubmit = async (e) => { // Defining a function to handle form submission
+        e.preventDefault(); // preventing default form submission behavoiur
+
+        const newPost = { // Creating a new post object
+            userId: user._id, // Assigning user ID to the new post
+            desc: desc.current.value, // Assigning the value of the input field to the new post's description
+        };
+
+        if (file) { // Checking if file exists
+            const Data = new FormData(); // Creating a new FormData object
+            const filename = Date.now() + file.name; // Generating a unique filename
+            Data.append("name", filename); // Adding the filename to the FormData object
+            Data.append("file", file); // Appending file to FormData
+           newPost.img = filename; // Adding filename to the new post object
+
+            // uplodaing he file to the server
+            try {
+                await axios.post("http://localhost:8800/api/upload", Data); // Uploading the file to the server    
+            } catch(error){
+                console.log(error) //  Logging any errors that occur during file upload
+            }
+            }
+
+            // creating a new Post 
+            try {
+                
+            }
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
--- selfsocial/src/components/sidebar/sidebar.jsx
+++ selfsocial/src/components/sidebar/sidebar.jsx
@@ -1,4 +1,82 @@
 import "./sidebar.css"
 import RssFeedIcon from '@mui/icons-material/RssFeed';
 import ChatIcon from '@mui/icons-material/Chat';
+import PlayCircleIcon from '@mui/icons-material/PlayCircleFilledOutlined';
+import GroupIcon from '@mui/icons-material/Group';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import WorkIcon from '@mui/icons-material/WorkOutline';
+import EventIcon from '@mui/icons-material/Event';
+import SchoolIcon from '@mui/icons-material/School';
+import { Dummydata } from "../../dummyData"
+import CloseFriend from "../closeFriend/closeFriend"
 
+export default function Sidebar() {
+    
+
+    return(
+        <div className="sidebar">
+            <div className="sidebarWrapper">
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <RssFeedIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Feed</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <ChatIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Chats</span>
+
+                    </li>
+                
+                    <li className="sidebarListItem">
+                        <PlayCircleIcon className="sidebarIcon" />
+                       <span className="sidebarListItemText">Videos</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <GroupIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Groups</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <BookmarkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Bookmarks</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <HelpOutlineIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Questions</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <WorkIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Jobs</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <EventIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Events</span>
+                    </li>
+
+                    <li className="sidebarListItem">
+                        <SchoolIcon className="sidebarIcon" />
+                        <span className="sidebarListItemText">Courses</span>
+                    </li>
+
+                    </ul>
+
+                    <button className="sidebarButton">Show More</button>
+                    <hr className="sidebarhr"/>
+                    <ul className="sidebarFriendList">
+
+                    {Dummydata.map((u) => (
+                        <CloseFriend key={u.id} user={u} />
+                    ))}
+                </ul>
+            </div>
+        </div>
+
+    )
+
+}
