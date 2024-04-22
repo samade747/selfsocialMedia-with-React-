6d5ef1c2f3c3b3190c28fb261a9327b85857b585
samade747 <samad.e747@gmail.com>
2024-04-22 14:53:53 +0500

    253

--- selfsocial/src/components/rightbar/rightbar.jsx
+++ selfsocial/src/components/rightbar/rightbar.jsx
@@ -2,7 +2,7 @@ import "./rightbar.css";
 import RedeemIcon from '@mui/icons-material/Redeem';
 import { Dummydata } from "../../dummyData";
 import Online from "../online/online";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, useImperativeHandle } from "react";
 import axios from "axios";
 import { AuthContext } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
@@ -15,7 +15,33 @@ export default function Rightbar({ user }) {
     const [followed, setfollowed] = useState(currentUser.followings.includes(user?._id));
 
     const handleclick = async () => {
-        
+        try{
+            if(followed){
+                await axios.put('http://localhost:8800/api/users/' + user._id + '/unfollow', {userId:currentUser._id})
+                dispatch({type:"UNFOLLOW",payload:user._id})
+            }
+            
+            else{
+                await axios.put('http://localhost:8800/api/users/' + user._id + '/follow', {userId:currentUser._id})
+                dispatch({type:"FOLLOW",payload:user._id})                
+            }
+        } catch(error) {
+                console.log(error);
+        }
+        setfollowed(!followed)
     }
     
 }
+
+useEffect(() => {
+    setfollowed(currentUser.followings.includes(user?._id))
+}, [currentUser, user?._id])
+
+useEffect(() => {
+    const getfriends = async () => {
+        
+    }
+
+})
+
+
